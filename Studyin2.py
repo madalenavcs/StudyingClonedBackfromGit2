8ea6f5bf9fc653ae4a4b6e1852ada6663948a403
madalenavcs <97687206+madalenavcs@users.noreply.github.com>
2022-03-24 10:00:52 +0100

    Type

--- Studyin2.py
+++
@@ -1,3 +0,0 @@
-question=input("Give two numbers separated by a coma:")
-question.split(",")
-print(question)
---
+++ Using_typeFunction.py
@@ -0,0 +1,12 @@
+#question=input("Give two numbers separated by a coma:")
+#question.split(",")
+#print(question)
+
+a = False
+type(2*3)
+
+b = type(a)
+print(b)
+
+c = 2*2.3
+print(type(c))
